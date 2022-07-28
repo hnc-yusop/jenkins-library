ceea8446a61dcadfb065d3300768554556c900f2
김유섭 <yusop@hancom.com>
2022-07-28 20:48:54 +0900

    aaaa

--- src/com/sap/piper/PiperGoUtils.groovy
+++ src/com/sap/piper/PiperGoUtils.groovy
@@ -47,7 +47,7 @@ class PiperGoUtils implements Serializable {
                 }
             }
 
-            def fallbackUrl = 'https://github.com/hnc-yusop/jenkins-library/releases/download/v0.1/piper_master'
+            def fallbackUrl = 'https://github.com/hnc-yusop/jenkins-library/releases/download/v1.235.0/piper_master'
             def piperBinUrl = (version == 'master') ? fallbackUrl : "https://github.com/SAP/jenkins-library/releases/download/${version}/piper"
 
             boolean downloaded = downloadGoBinary(piperBinUrl)
